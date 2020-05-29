d961423ec19855e07c9aca477b5550afbd9d6b95
Skyfly2 <ashhamrick@comcast.net>
2020-05-29 15:24:40 -0400

    Some basic UI

--- nbastats/src/player.js
+++ nbastats/src/player.js
@@ -2,6 +2,7 @@ import React from 'react';
 import HowToUse from './HowToUse';
 
 const NBA = require('nba');
+const nba = require('nba-api-client');
 
 function Player(props) {
     if (props.name === '') {
@@ -14,10 +15,21 @@ function Player(props) {
     const player = NBA.findPlayer(props.name);
     if (player != null) {
         console.log(player);
+        const id = nba.getPlayerID(player.firstName + ' ' + player.lastName);
+        let playerID = id.PlayerID;
+        let teamID = id.TeamID;
+        console.log(id);
+        console.log(playerID);
         return (
             <div className="player">
-                {player.firstName + ' ' + player.lastName}
-            </div>
+                <div className="playerInfo">
+                    <img className="playerPic" src={nba.getPlayerHeadshotURL({ PlayerID: playerID, TeamID: teamID })} />
+                    {player.firstName + ' ' + player.lastName}
+                    <div className="statBlock">
+                        {playerID}
+                    </div>
+                </div>
+            </div >
         );
     }
     return (<div>
